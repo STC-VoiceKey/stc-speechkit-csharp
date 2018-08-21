432aaab116ebb82166ab9fbc430851750f7b7678
sadurtinova <sadurtinova@speechpro.com>
2018-08-21 14:07:55 +0300

    C# SpeechCloud client
    C# lib for STC SpeechCloud generated by swagger codegen.

---
+++ diarization/ConcoleTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using Cloud.Client.ASR.Model;
+using Cloud.Client.ASR.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SessionApi sessionApi = new SessionApi();
+            StartSessionRequest startSession = new StartSessionRequest("user", "password", 290);
+            var response = sessionApi.StartSession(startSession);
+            Guid? sessionId = response.SessionId;
+        }
+    }
+}
