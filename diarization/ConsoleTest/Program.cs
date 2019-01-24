7eeabc59d62b99c9f879c5b4ddf20407392b3347
sadurtinova <sadurtinova@speechpro.com>
2019-01-24 17:59:00 +0300

    Updated example for version 1.1

--- diarization/ConsoleTest/Program.cs
+++ diarization/ConsoleTest/Program.cs
@@ -10,17 +10,15 @@ namespace ConcoleTest
         static void Main(string[] args)
         {
             var sessionApi = new SessionApi();
-            var startSession = new AuthRequestDto("danilov-k@speechpro.com", 261, "!CPa490c71");
+            var startSession = new AuthRequestDto("user", 261, "password");
             var response = sessionApi.Login(startSession);
             var sessionId = response.SessionId;
-            Console.WriteLine(sessionId);
-            sessionApi.Logout(new Guid(sessionId));
-            Console.ReadLine();
-            /*var diarizationApi = new DiarizationApi();
-            var sound = File.ReadAllBytes("F:\\Cloud\\3.2\\swagger\\diarization\\ira_8khz.wav");
+            var diarizationApi = new DiarizationApi();
+            var sound = File.ReadAllBytes("F:\\Cloud\\diarization\\8khz.wav");
             var audio = new AudioDto(sound);
             var diarizationResponse = diarizationApi.Diarization(Guid.Parse(sessionId), audio);
-            var speakers = diarizationResponse.Speakers;*/
+            var speakers = diarizationResponse.Speakers;
+            speakers.ForEach(Console.WriteLine);
         }
     }
 }
